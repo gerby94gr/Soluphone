f91151bf67c595cede87d3800511f9e1eda4af75
German Rodriguez <gerby94gr@gmail.com>
2026-05-17 20:01:38 -0300

    Fix customers page

---
+++ app/customers/page.tsx
@@ -0,0 +1,18 @@
+export default function CustomersPage() {
+  return (
+    <div
+      style={{
+        padding: 30,
+        color: "white",
+      }}
+    >
+      <h1>
+        Clientes
+      </h1>
+
+      <p>
+        Gestión de clientes próximamente
+      </p>
+    </div>
+  );
+}
